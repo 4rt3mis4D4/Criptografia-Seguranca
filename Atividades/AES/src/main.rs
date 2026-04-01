95bcef07e1ef434605a6423ff868d56e85cc3e8f
Artemis <130113421+4rt3mis4D4@users.noreply.github.com>
2026-04-01 16:34:41 -0300

    Passing the encryption function to the main method and creating decryption functions.

--- Atividades/AES/src/main.rs
+++ Atividades/AES/src/main.rs
@@ -1,5 +1,3 @@
-use std::io::{self, Write}; // Biblioteca para entrada e saída
-
 // Tabela S-Box - Acabar com padrões:
 // ??? Usada na função SubBytes para substituir cada byte do estado
 // ??? Projetada para ser não-linear e resistente a criptoanálise
@@ -21,6 +19,26 @@ const S_Box: [u8; 256] =
 0xe1,0xf8,0x98,0x11,0x69,0xd9,0x8e,0x94,0x9b,0x1e,0x87,0xe9,0xce,0x55,0x28,0xdf,
 0x8c,0xa1,0x89,0x0d,0xbf,0xe6,0x42,0x68,0x41,0x99,0x2d,0x0f,0xb0,0x54,0xbb,0x16];
 
+// Tabela S-Box inverso (DESCRIPTOGRAFAR)
+const S_Box_Inv: [u8; 256] = [
+    0x52, 0x09, 0x6a, 0xd5, 0x30, 0x36, 0xa5, 0x38, 0xbf, 0x40, 0xa3, 0x9e, 0x81, 0xf3, 0xd7, 0xfb,
+    0x7c, 0xe3, 0x39, 0x82, 0x9b, 0x2f, 0xff, 0x87, 0x34, 0x8e, 0x43, 0x44, 0xc4, 0xde, 0xe9, 0xcb,
+    0x54, 0x7b, 0x94, 0x32, 0xa6, 0xc2, 0x23, 0x3d, 0xee, 0x4c, 0x95, 0x0b, 0x42, 0xfa, 0xc3, 0x4e,
+    0x08, 0x2e, 0xa1, 0x66, 0x28, 0xd9, 0x24, 0xb2, 0x76, 0x5b, 0xa2, 0x49, 0x6d, 0x8b, 0xd1, 0x25,
+    0x72, 0xf8, 0xf6, 0x64, 0x86, 0x68, 0x98, 0x16, 0xd4, 0xa4, 0x5c, 0xcc, 0x5d, 0x65, 0xb6, 0x92,
+    0x6c, 0x70, 0x48, 0x50, 0xfd, 0xed, 0xb9, 0xda, 0x5e, 0x15, 0x46, 0x57, 0xa7, 0x8d, 0x9d, 0x84,
+    0x90, 0xd8, 0xab, 0x00, 0x8c, 0xbc, 0xd3, 0x0a, 0xf7, 0xe4, 0x58, 0x05, 0xb8, 0xb3, 0x45, 0x06,
+    0xd0, 0x2c, 0x1e, 0x8f, 0xca, 0x3f, 0x0f, 0x02, 0xc1, 0xaf, 0xbd, 0x03, 0x01, 0x13, 0x8a, 0x6b,
+    0x3a, 0x91, 0x11, 0x41, 0x4f, 0x67, 0xdc, 0xea, 0x97, 0xf2, 0xcf, 0xce, 0xf0, 0xb4, 0xe6, 0x73,
+    0x96, 0xac, 0x74, 0x22, 0xe7, 0xad, 0x35, 0x85, 0xe2, 0xf9, 0x37, 0xe8, 0x1c, 0x75, 0xdf, 0x6e,
+    0x47, 0xf1, 0x1a, 0x71, 0x1d, 0x29, 0xc5, 0x89, 0x6f, 0xb7, 0x62, 0x0e, 0xaa, 0x18, 0xbe, 0x1b,
+    0xfc, 0x56, 0x3e, 0x4b, 0xc6, 0xd2, 0x79, 0x20, 0x9a, 0xdb, 0xc0, 0xfe, 0x78, 0xcd, 0x5a, 0xf4,
+    0x1f, 0xdd, 0xa8, 0x33, 0x88, 0x07, 0xc7, 0x31, 0xb1, 0x12, 0x10, 0x59, 0x27, 0x80, 0xec, 0x5f,
+    0x60, 0x51, 0x7f, 0xa9, 0x19, 0xb5, 0x4a, 0x0d, 0x2d, 0xe5, 0x7a, 0x9f, 0x93, 0xc9, 0x9c, 0xef,
+    0xa0, 0xe0, 0x3b, 0x4d, 0xae, 0x2a, 0xf5, 0xb0, 0xc8, 0xeb, 0xbb, 0x3c, 0x83, 0x53, 0x99, 0x61,
+    0x17, 0x2b, 0x04, 0x7e, 0xba, 0x77, 0xd6, 0x26, 0xe1, 0x69, 0x14, 0x63, 0x55, 0x21, 0x0c, 0x7d,
+];
+
 // RCON - Constantes de rodada:
 // ??? Usadas na expansão de chave para evitar simetrias
 const RCON: [u8; 11] = [0x00, 0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80, 0x1b, 0x36];
@@ -30,6 +48,13 @@ const RCON: [u8; 11] = [0x00, 0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80, 0x
 fn sub_bytes(state: &mut [u8; 16]){ 
     for i in 0..16 { // --- Para cada posição no array de 16 bytes
         state[i] = S_Box[state[i] as usize]; // --- Substitui o byte atual pelo valor da SBOX na posição do byte
+    }
+}
+
+// 1.2 Função SubBytes Inverse (DESCRIPTOGRAFAR)
+fn inv_sub_bytes(state: &mut [u8; 16]) {
+    for i in 0..16 {
+        state[i] = S_Box_Inv[state[i] as usize]; 
     }
 }
 
@@ -60,6 +85,36 @@ fn shift_rows(state: &mut[u8; 16]){
     state[11] = state[7];
     state[7] = state[3];
     state[3] = temp;
+}
+
+// 2.2 Função ShiftRows Inverse (DESCRIPTOGRAFAR)
+// --- Desloca para a direita
+fn inv_shift_rows(state: &mut [u8; 16]){
+    
+    // --- Desloca 1 posição para a direita
+    let temp = state[13];
+
+    state[13] = state[9];
+    state[9] = state[5];
+    state[5] = state[1];
+    state[1] = temp;
+
+    // --- Desloca 2 posições para a direita
+    let temp1 = state[14];
+    let temp2 = state[10];
+
+    state[14] = state[6];
+    state[10] = state[2];
+    state[6] = temp1;
+    state[2] = temp2;
+
+    // --- Desloca 3 posições para a direita
+    let temp = state[3];
+
+    state[3] = state[7];
+    state[7] = state[11];
+    state[11] = state[15];
+    state[15] = temp;
 }
 
 // 3.0 Função Multiplicação no Campo de Galois
@@ -105,9 +160,36 @@ fn mix_columns(state: &mut [u8; 16]){
 
         // --- Aplica a transformação matricial no campo GF(2^8)
         state[idx] = galois_mult(0x02, s0) ^ galois_mult(0x03, s1) ^ s2 ^ s3;
+
         state[idx + 1] = s0 ^ galois_mult(0x02, s1) ^ galois_mult(0x03, s2) ^ s3;
+
         state[idx + 2] = s0 ^ s1 ^ galois_mult(0x02, s2) ^ galois_mult(0x03, s3);
+
         state[idx + 3] = galois_mult(0x03, s0) ^ s1 ^ s2 ^ galois_mult(0x02, s3);
+    }
+}
+
+// 3.2 Função MixColumns Inverse (DESCRIPTOGRAFAR)
+fn inv_mix_columns(state: &mut [u8; 16]){
+    for col in 0..4 {
+        let idx = col * 4;
+
+        let s0 = state[idx];
+        let s1 = state[idx + 1];
+        let s2 = state[idx + 2];
+        let s3 = state[idx + 3];
+
+        state[idx] = galois_mult(0x0E, s0) ^ galois_mult(0x0B, s1) ^ 
+                    galois_mult(0x0D, s2) ^ galois_mult(0x09, s3);
+
+        state[idx + 1] = galois_mult(0x09, s0) ^ galois_mult(0x0E, s1) ^
+                        galois_mult(0x0B, s2) ^ galois_mult(0x0D, s3);
+
+        state[idx + 2] = galois_mult(0x0D, s0) ^ galois_mult(0x09, s1) ^ 
+                        galois_mult(0x0E, s2) ^ galois_mult(0x0B, s3);
+
+        state[idx + 3] = galois_mult(0x0B, s0) ^ galois_mult(0x0D, s1) ^ 
+                        galois_mult(0x09, s2) ^ galois_mult(0x0E, s3);
     }
 }
 
@@ -177,67 +259,101 @@ fn key_expansion(key: &[u8; 32]) -> [[u8; 16]; 15]{
             round_keys[round][j * 4 + 3] = palavras[palavra_idx][3];
         }
     }
-
     round_keys
-}
-
-// === FUNÇÃO DE CRIPTOGRAFIA ===
-fn criptografar(block: &mut [u8; 16], round_keys: &[[u8; 16];15]){
-    add_round_key(block, &round_keys[0]); // Rodada inicial
-
-    // 14 rodadas principais (1 a 13)
-    for round in 1..13 {
-        sub_bytes(block);
-        shift_rows(block);
-        mix_columns(block);
-        add_round_key(block, &round_keys[round]);
-    }
-
-    // Rodada final - [!] Sem MixColumns
-    sub_bytes(block);
-    shift_rows(block);
-    add_round_key(block, &round_keys[14]);
 }
 
 // === MAIN ===
 fn main(){
-    println!("=== AES-256 ===");
-    println!();
+    use std::fs::File; // Biblioteca sistema de arquivos
+    use std::io::{self, Read, Write};  // Biblioteca de entrada e saída de dados
 
-    // Solicitação de senha
-    print!("Digite a senha (32 bytes / 32 caracteres): ");
-    io::stdout().flush().unwrap(); // Força exibição
+    println!("=== AES-256 ===");
+
+    // 1. Configuração da chave
+    println!("Digite a senha de 32 caracteres: ");
+    io::stdout().flush().unwrap();
 
     let mut senha = String::new();
-    io::stdin().read_line(&mut senha).expect("Falha ao ler senha.");
-    let senha = senha.trim(); // Remove espaços e quebras de linha
+    io::stdin().read_line(&mut senha).expect("Falha ao ler senha");
+    let senha = senha.trim(); // ---Remove espaços e quebras de linha
 
-    // Verifica se a senha tem 32 bytes
+        // ---Verifica se a senha tem 32 bytes / 32 caracteres
     if senha.as_bytes().len() != 32 {
         println!("[!] ERRO: A senha deve ter exatamente 32 caracteres.");
         return;
     }
 
-    // Converte a senha para um array de 32 bytes
+        // ---Converte a senha para um array de 32 bytes
     let mut key = [0u8; 32];
     key.copy_from_slice(&senha.as_bytes()[..32]);
 
-    // Senha configurada com sucesso
-    println!("Senha configurada com sucesso!");
-    println!("Chave (hex): {}", hex::encode(&key));
-    println!();
-
-    // Expade a chave para gerar 15 chaves das rodadas
-    println!("Expandindo chave para 15 rodadas...");
+    // 2. Expansão da chave
     let round_keys = key_expansion(&key);
-    println!("Expansão concluída!");
 
-    // Texto de exemplo para criptografar
-    let mut block = [0u8; 16];
-    let plaintext = b"Exemplo AES-256!";
-    block.copy_from_slice(&plaintext[..16]);
+    // 3. Leitura do arquivo "mensagem.txt"
+    //let mut block = [0u8; 16];
+    let mut file_in = File::open("mensagem.txt").expect("Não foi possível abrir o arquivo.");
+    let mut file_out = File::create("mensagem_criptografada.txt").expect("Falha ao criar arquivo");
 
-    println!("Original: {:02x?}", block);
-    criptografar(&mut block, &round_keys);
-    println!("Criptografado: {:02x?}", block);
+    //file_in.read_exact(&mut block).expect("O arquivo deve ter exatamente 16 bytes.");
+    //println!("Conteúdo original (hex): {:02x?}", block);
+
+    // 4. === Criptografia ===
+    let mut buffer = [0u8; 16];
+
+    println!("Criptografando arquivo...");
+
+    while let Ok(n) = file_in.read(&mut buffer){
+        if n == 0 { break; } // Fim do arquivo
+
+        add_round_key(&mut buffer, &round_keys[0]); // Adição da chave inicial
+
+            // ---13 rodadas
+        for i in 1..14 {
+            sub_bytes(&mut buffer);
+            shift_rows(&mut buffer);
+            mix_columns(&mut buffer);
+            add_round_key(&mut buffer, &round_keys[i]);
+        }
+
+            // ---rodada final (sem mixcolumn)
+        sub_bytes(&mut buffer);
+        shift_rows(&mut buffer);
+        add_round_key(&mut buffer, &round_keys[14]);
+
+        // 5. Salvar resultado .txt
+        file_out.write_all(&buffer).expect("Falha ao escrever um arquivo.");
+    }
+    println!("Criptografado com sucesso em 'mensagem_criptografada.txt'!");
+
+    // Leitura do arquivo criptografado
+    let mut file_in_cript = File::open("mensagem_criptografada.txt").expect("Não foi possível abrir o arquivo.");
+
+    let mut file_out = File::create("mensagem_descriptografada.txt").expect("Falha ao criar arquivo");
+
+    // 6. === Descriptografia ===
+    println!("Descriptografando arquivo...");
+
+    while let Ok(n) = file_in_cript.read(&mut buffer) {
+        if n == 0 { break; } // Fim do arquivo
+
+        add_round_key(&mut buffer, &round_keys[14]); // Adição da chave inicial
+
+            // ---13 rodadas
+        for i in (1..14).rev() {
+            inv_sub_bytes(&mut buffer);
+            inv_shift_rows(&mut buffer);
+            add_round_key(&mut buffer, &round_keys[i]);
+            inv_mix_columns(&mut buffer);
+        }
+
+            // ---rodada final (sem mixcolumn)
+        inv_sub_bytes(&mut buffer);
+        inv_shift_rows(&mut buffer);
+        add_round_key(&mut buffer, &round_keys[0]);
+
+            // 7. Salvar resultado .txt
+        file_out.write_all(&buffer).expect("Falha ao escrever um arquivo.");
+    }
+    println!("Descriptografado com sucesso em 'mensagem_descriptografada.txt'!");
 }
