5a79d10e511566cfe3c8a9aef54c6be3a724e81e
Artemis <130113421+4rt3mis4D4@users.noreply.github.com>
2026-04-01 21:03:53 -0300

    AES's optimized structured infrastructure, following activity criteria.

--- Atividades/AES/src/main.rs
+++ Atividades/AES/src/main.rs
@@ -1,27 +1,36 @@
+// === BIBLIOTECAS ===
+    use std::fs::File; // Biblioteca sistema de arquivos
+    use std::io::{self, Read, Write};  // Biblioteca de entrada e saída de dados
+    use std::env; // Biblioteca para instruções externas (terminal)
+    use hmac::{Hmac, Mac}; // Biblioteca que gera um código de autenticação usando uma chave e um hash
+    use sha2::Sha256; // Biblioteca usada para transformar dados em uma "impressão digital" única de 256 bits 
+
+    type HmacSha256 = Hmac<Sha256>; // tipo auxiliar
+
 // Tabela S-Box - Acabar com padrões:
 // ??? Usada na função SubBytes para substituir cada byte do estado
 // ??? Projetada para ser não-linear e resistente a criptoanálise
 const S_Box: [u8; 256] = 
-[0x63,0x7c,0x77,0x7b,0xf2,0x6b,0x6f,0xc5,0x30,0x01,0x67,0x2b,0xfe,0xd7,0xab,0x76,
-0xca,0x82,0xc9,0x7d,0xfa,0x59,0x47,0xf0,0xad,0xd4,0xa2,0xaf,0x9c,0xa4,0x72,0xc0,
-0xb7,0xfd,0x93,0x26,0x36,0x3f,0xf7,0xcc,0x34,0xa5,0xe5,0xf1,0x71,0xd8,0x31,0x15,
-0x04,0xc7,0x23,0xc3,0x18,0x96,0x05,0x9a,0x07,0x12,0x80,0xe2,0xeb,0x27,0xb2,0x75,
-0x09,0x83,0x2c,0x1a,0x1b,0x6e,0x5a,0xa0,0x52,0x3b,0xd6,0xb3,0x29,0xe3,0x2f,0x84,
-0x53,0xd1,0x00,0xed,0x20,0xfc,0xb1,0x5b,0x6a,0xcb,0xbe,0x39,0x4a,0x4c,0x58,0xcf,
-0xd0,0xef,0xaa,0xfb,0x43,0x4d,0x33,0x85,0x45,0xf9,0x02,0x7f,0x50,0x3c,0x9f,0xa8,
-0x51,0xa3,0x40,0x8f,0x92,0x9d,0x38,0xf5,0xbc,0xb6,0xda,0x21,0x10,0xff,0xf3,0xd2,
-0xcd,0x0c,0x13,0xec,0x5f,0x97,0x44,0x17,0xc4,0xa7,0x7e,0x3d,0x64,0x5d,0x19,0x73,
-0x60,0x81,0x4f,0xdc,0x22,0x2a,0x90,0x88,0x46,0xee,0xb8,0x14,0xde,0x5e,0x0b,0xdb,
-0xe0,0x32,0x3a,0x0a,0x49,0x06,0x24,0x5c,0xc2,0xd3,0xac,0x62,0x91,0x95,0xe4,0x79,
-0xe7,0xc8,0x37,0x6d,0x8d,0xd5,0x4e,0xa9,0x6c,0x56,0xf4,0xea,0x65,0x7a,0xae,0x08,
-0xba,0x78,0x25,0x2e,0x1c,0xa6,0xb4,0xc6,0xe8,0xdd,0x74,0x1f,0x4b,0xbd,0x8b,0x8a,
-0x70,0x3e,0xb5,0x66,0x48,0x03,0xf6,0x0e,0x61,0x35,0x57,0xb9,0x86,0xc1,0x1d,0x9e,
-0xe1,0xf8,0x98,0x11,0x69,0xd9,0x8e,0x94,0x9b,0x1e,0x87,0xe9,0xce,0x55,0x28,0xdf,
-0x8c,0xa1,0x89,0x0d,0xbf,0xe6,0x42,0x68,0x41,0x99,0x2d,0x0f,0xb0,0x54,0xbb,0x16];
+    [0x63,0x7c,0x77,0x7b,0xf2,0x6b,0x6f,0xc5,0x30,0x01,0x67,0x2b,0xfe,0xd7,0xab,0x76,
+    0xca,0x82,0xc9,0x7d,0xfa,0x59,0x47,0xf0,0xad,0xd4,0xa2,0xaf,0x9c,0xa4,0x72,0xc0,
+    0xb7,0xfd,0x93,0x26,0x36,0x3f,0xf7,0xcc,0x34,0xa5,0xe5,0xf1,0x71,0xd8,0x31,0x15,
+    0x04,0xc7,0x23,0xc3,0x18,0x96,0x05,0x9a,0x07,0x12,0x80,0xe2,0xeb,0x27,0xb2,0x75,
+    0x09,0x83,0x2c,0x1a,0x1b,0x6e,0x5a,0xa0,0x52,0x3b,0xd6,0xb3,0x29,0xe3,0x2f,0x84,
+    0x53,0xd1,0x00,0xed,0x20,0xfc,0xb1,0x5b,0x6a,0xcb,0xbe,0x39,0x4a,0x4c,0x58,0xcf,
+    0xd0,0xef,0xaa,0xfb,0x43,0x4d,0x33,0x85,0x45,0xf9,0x02,0x7f,0x50,0x3c,0x9f,0xa8,
+    0x51,0xa3,0x40,0x8f,0x92,0x9d,0x38,0xf5,0xbc,0xb6,0xda,0x21,0x10,0xff,0xf3,0xd2,
+    0xcd,0x0c,0x13,0xec,0x5f,0x97,0x44,0x17,0xc4,0xa7,0x7e,0x3d,0x64,0x5d,0x19,0x73,
+    0x60,0x81,0x4f,0xdc,0x22,0x2a,0x90,0x88,0x46,0xee,0xb8,0x14,0xde,0x5e,0x0b,0xdb,
+    0xe0,0x32,0x3a,0x0a,0x49,0x06,0x24,0x5c,0xc2,0xd3,0xac,0x62,0x91,0x95,0xe4,0x79,
+    0xe7,0xc8,0x37,0x6d,0x8d,0xd5,0x4e,0xa9,0x6c,0x56,0xf4,0xea,0x65,0x7a,0xae,0x08,
+    0xba,0x78,0x25,0x2e,0x1c,0xa6,0xb4,0xc6,0xe8,0xdd,0x74,0x1f,0x4b,0xbd,0x8b,0x8a,
+    0x70,0x3e,0xb5,0x66,0x48,0x03,0xf6,0x0e,0x61,0x35,0x57,0xb9,0x86,0xc1,0x1d,0x9e,
+    0xe1,0xf8,0x98,0x11,0x69,0xd9,0x8e,0x94,0x9b,0x1e,0x87,0xe9,0xce,0x55,0x28,0xdf,
+    0x8c,0xa1,0x89,0x0d,0xbf,0xe6,0x42,0x68,0x41,0x99,0x2d,0x0f,0xb0,0x54,0xbb,0x16];
 
 // Tabela S-Box inverso (DESCRIPTOGRAFAR)
-const S_Box_Inv: [u8; 256] = [
-    0x52, 0x09, 0x6a, 0xd5, 0x30, 0x36, 0xa5, 0x38, 0xbf, 0x40, 0xa3, 0x9e, 0x81, 0xf3, 0xd7, 0xfb,
+const S_Box_Inv: [u8; 256] = 
+    [0x52, 0x09, 0x6a, 0xd5, 0x30, 0x36, 0xa5, 0x38, 0xbf, 0x40, 0xa3, 0x9e, 0x81, 0xf3, 0xd7, 0xfb,
     0x7c, 0xe3, 0x39, 0x82, 0x9b, 0x2f, 0xff, 0x87, 0x34, 0x8e, 0x43, 0x44, 0xc4, 0xde, 0xe9, 0xcb,
     0x54, 0x7b, 0x94, 0x32, 0xa6, 0xc2, 0x23, 0x3d, 0xee, 0x4c, 0x95, 0x0b, 0x42, 0xfa, 0xc3, 0x4e,
     0x08, 0x2e, 0xa1, 0x66, 0x28, 0xd9, 0x24, 0xb2, 0x76, 0x5b, 0xa2, 0x49, 0x6d, 0x8b, 0xd1, 0x25,
@@ -36,8 +45,7 @@ const S_Box_Inv: [u8; 256] = [
     0x1f, 0xdd, 0xa8, 0x33, 0x88, 0x07, 0xc7, 0x31, 0xb1, 0x12, 0x10, 0x59, 0x27, 0x80, 0xec, 0x5f,
     0x60, 0x51, 0x7f, 0xa9, 0x19, 0xb5, 0x4a, 0x0d, 0x2d, 0xe5, 0x7a, 0x9f, 0x93, 0xc9, 0x9c, 0xef,
     0xa0, 0xe0, 0x3b, 0x4d, 0xae, 0x2a, 0xf5, 0xb0, 0xc8, 0xeb, 0xbb, 0x3c, 0x83, 0x53, 0x99, 0x61,
-    0x17, 0x2b, 0x04, 0x7e, 0xba, 0x77, 0xd6, 0x26, 0xe1, 0x69, 0x14, 0x63, 0x55, 0x21, 0x0c, 0x7d,
-];
+    0x17, 0x2b, 0x04, 0x7e, 0xba, 0x77, 0xd6, 0x26, 0xe1, 0x69, 0x14, 0x63, 0x55, 0x21, 0x0c, 0x7d];
 
 // RCON - Constantes de rodada:
 // ??? Usadas na expansão de chave para evitar simetrias
@@ -61,60 +69,54 @@ fn inv_sub_bytes(state: &mut [u8; 16]) {
 // 2. Função ShiftRows 
 // ??? Desloca cada linha da matriz para a esquerda
 fn shift_rows(state: &mut[u8; 16]){
+    let mut temp = [0u8; 16];
+    temp.copy_from_slice(state);
+    
     // Coluna 1
-    let temp = state[1]; // --- Salva primeiro elemento
     // --- Desloca 1 posição para a esquerda
-    state[1] = state[5];
-    state[5] = state[9];
-    state[9] = state[13];
-    state[13] = temp;
+    state[1] = temp[5];
+    state[5] = temp[9];
+    state[9] = temp[13];
+    state[13] = temp[1];
 
     // Coluna 2
-    let temp1 = state[2]; // --- Salva primeiro elemento
-    let temp2 = state[6]; // --- Salva segundo elemento
     // --- Desloca 2 posições para a esquerda
     state[2] = state[10];
     state[6] = state[14];
-    state[10] = temp1;
-    state[14] = temp2;
+    state[10] = temp[2];
+    state[14] = temp[6];
 
-    // Colina 3
-    let temp = state[15]; // --- Salva quarto elemento
+    // Coluna 3
     // --- Desloca 3 posições para a esquerda
-    state[15] = state[11];
-    state[11] = state[7];
-    state[7] = state[3];
-    state[3] = temp;
+    state[3] = temp[15];
+    state[7] = temp[3];
+    state[11] = temp[7];
+    state[15] = temp[11];
 }
 
 // 2.2 Função ShiftRows Inverse (DESCRIPTOGRAFAR)
 // --- Desloca para a direita
 fn inv_shift_rows(state: &mut [u8; 16]){
-    
-    // --- Desloca 1 posição para a direita
-    let temp = state[13];
+    let mut temp = [0u8; 16];
+    temp.copy_from_slice(state);
 
-    state[13] = state[9];
-    state[9] = state[5];
-    state[5] = state[1];
-    state[1] = temp;
+    // --- Desloca 1 posição para a direita
+    state[1] = temp[13];
+    state[5] = temp[1];
+    state[9] = temp[5];
+    state[13] = temp[9];
 
     // --- Desloca 2 posições para a direita
-    let temp1 = state[14];
-    let temp2 = state[10];
-
-    state[14] = state[6];
-    state[10] = state[2];
-    state[6] = temp1;
-    state[2] = temp2;
+    state[2] = temp[10];
+    state[6] = temp[14];
+    state[10] = temp[2];
+    state[14] = temp[6];
 
     // --- Desloca 3 posições para a direita
-    let temp = state[3];
-
-    state[3] = state[7];
-    state[7] = state[11];
-    state[11] = state[15];
-    state[15] = temp;
+    state[3] = temp[7];
+    state[7] = temp[11];
+    state[11] = temp[15];
+    state[15] = temp[3];
 }
 
 // 3.0 Função Multiplicação no Campo de Galois
@@ -225,15 +227,14 @@ fn key_expansion(key: &[u8; 32]) -> [[u8; 16]; 15]{
         if i % 8 == 0{
             // --- RotWord: rotação circular de 1 byte para esquerda
             temp = [temp[1], temp[2], temp[3], temp[0]];
-
             // --- SubWord: aplica SBOX em cada byte
             for j in 0..4 {
                 temp[j] = S_Box[temp[j] as usize];
             }
-
             // --- XOR com a constante RCON
             temp[0] ^= RCON[i / 8];
         }
+
         // --- Para AES-256, a cada 8 palavras também aplica SubWord
         else if i % 8 == 4 {
             for j in 0..4 {
@@ -262,98 +263,204 @@ fn key_expansion(key: &[u8; 32]) -> [[u8; 16]; 15]{
     round_keys
 }
 
-// === MAIN ===
-fn main(){
-    use std::fs::File; // Biblioteca sistema de arquivos
-    use std::io::{self, Read, Write};  // Biblioteca de entrada e saída de dados
+// === PBKDF2 ===
+// ??? Transforma senha simples em chave forte
+fn pbkdf2(pass: &str, salt: &[u8; 16], iter: u32) -> [u8; 32]{
+    let mut result = [0u8; 32]; // Armazena resultado final
 
-    println!("=== AES-256 ===");
+    let pass_bytes = pass.as_bytes(); // Converte string em bytes
+    let mut salt_concat = salt.to_vec(); // Cria um vetor mutável
+    salt_concat.extend_from_slice(&1u32.to_be_bytes()); // Concatena o salt com o número do bloco
 
-    // 1. Configuração da chave
-    println!("Digite a senha de 32 caracteres: ");
+    // --- Inicializa o motor HMAC usando SHA-256, utilizando a senha como a chave secreta
+    let mut mac = HmacSha256::new_from_slice(pass_bytes).unwrap();
+    // ---Passa para o HMAC o salt ja concatenado
+    mac.update(&salt_concat);
+    // --- Finaliza o cálculo e armazena o primeiro hash resultante (U1)
+    let mut u = mac.finalize().into_bytes();
+    result.copy_from_slice(&u); // Copia o valor
+
+    for _ in 1..iter { // Roda até o número de iteração
+        // --- Reinicia o HMAC com a senha para a nova rodada.
+        let mut mac = HmacSha256::new_from_slice(pass_bytes).unwrap();
+        mac.update(&u); 
+        u = mac.finalize().into_bytes(); // Calcula o novo hash e sobrescreve u
+        for j in 0..32 {
+            result[j] ^= u[j]; // Operação XOR
+        }
+    }
+    result 
+}
+
+// === CRIPTOGRAFAR ===
+// Modo de operação CBC (Cipher Black Chaining)
+// ??? Cada bloco de texto simples passa por uma operação XOR com o bloco cifrado anterior antes de ser criptografado
+fn cifrar(caminho: &str){
+    print!("Digite a senha: "); // Solicitação de senha
     io::stdout().flush().unwrap();
 
-    let mut senha = String::new();
-    io::stdin().read_line(&mut senha).expect("Falha ao ler senha");
-    let senha = senha.trim(); // ---Remove espaços e quebras de linha
+    let mut senha = String::new(); // Salva senha
+    io::stdin().read_line(&mut senha).unwrap();
 
-        // ---Verifica se a senha tem 32 bytes / 32 caracteres
-    if senha.as_bytes().len() != 32 {
-        println!("[!] ERRO: A senha deve ter exatamente 32 caracteres.");
-        return;
-    }
+    let salt = [0xAFu8; 16]; // Valor fixo para salt (PBKDF2)
+    let iv = [0x12u8; 16]; // Valor fixo IV (CBC)
 
-        // ---Converte a senha para um array de 32 bytes
-    let mut key = [0u8; 32];
-    key.copy_from_slice(&senha.as_bytes()[..32]);
+    // Gera chave de 256 bits a partir da senha
+    let chave = pbkdf2(senha.trim(), &salt, 100_000);
+    let rkeys = key_expansion(&chave); // Expansão da chave
 
-    // 2. Expansão da chave
-    let round_keys = key_expansion(&key);
+    let mut f_in = File::open(caminho).unwrap(); // Abre o arquivo "mensagem.txt"
+    let mut f_out = File::create(format!("{}.cifrado", caminho)).unwrap(); // Novo arquivo criptografado
+    
+    // Escreva o Salt e o IV no ínicio do arquivo (quais valores serão usados?)
+    f_out.write_all(&salt).unwrap();
+    f_out.write_all(&iv).unwrap();
 
-    // 3. Leitura do arquivo "mensagem.txt"
-    //let mut block = [0u8; 16];
-    let mut file_in = File::open("mensagem.txt").expect("Não foi possível abrir o arquivo.");
-    let mut file_out = File::create("mensagem_criptografada.txt").expect("Falha ao criar arquivo");
+    let mut buffer = [0u8; 16]; // Bloco de 16 bytes
+    let mut iv_corr = iv; // Atualizado a cada bloco
 
-    //file_in.read_exact(&mut block).expect("O arquivo deve ter exatamente 16 bytes.");
-    //println!("Conteúdo original (hex): {:02x?}", block);
+    // Lê o arquivo em pedaços de 16 bytes
+    while let Ok(n) = f_in.read(&mut buffer){
+        if n == 0 { 
+            break;
+        }
 
-    // 4. === Criptografia ===
-    let mut buffer = [0u8; 16];
+        for i in 0..16 {
+            buffer[i] ^= iv_corr[i]; // Realiza o XOR
+        }
 
-    println!("Criptografando arquivo...");
+        // Processo de Criptografia
+        add_round_key(&mut buffer, &rkeys[0]);
 
-    while let Ok(n) = file_in.read(&mut buffer){
-        if n == 0 { break; } // Fim do arquivo
-
-        add_round_key(&mut buffer, &round_keys[0]); // Adição da chave inicial
-
-            // ---13 rodadas
-        for i in 1..14 {
+        // 13 Rodadas
+        for i in 1..14{
             sub_bytes(&mut buffer);
             shift_rows(&mut buffer);
             mix_columns(&mut buffer);
-            add_round_key(&mut buffer, &round_keys[i]);
+            add_round_key(&mut buffer, &rkeys[i]);
         }
 
-            // ---rodada final (sem mixcolumn)
+        // Rodada Final (14)
         sub_bytes(&mut buffer);
         shift_rows(&mut buffer);
-        add_round_key(&mut buffer, &round_keys[14]);
-
-        // 5. Salvar resultado .txt
-        file_out.write_all(&buffer).expect("Falha ao escrever um arquivo.");
+        add_round_key(&mut buffer, &rkeys[14]);
+        
+        iv_corr = buffer; // Salva resultado para ser usado no prox bloco
+        f_out.write_all(&buffer).unwrap(); // Grava os 16 byres cifrados do arq de saída
     }
-    println!("Criptografado com sucesso em 'mensagem_criptografada.txt'!");
+    println!("Cifrado com sucesso!");
+}
 
-    // Leitura do arquivo criptografado
-    let mut file_in_cript = File::open("mensagem_criptografada.txt").expect("Não foi possível abrir o arquivo.");
+fn decifrar(caminho: &str){
+    print!("Senha: "); // Solicita a senha para o usuário
+    io::stdout().flush().unwrap();
 
-    let mut file_out = File::create("mensagem_descriptografada.txt").expect("Falha ao criar arquivo");
+    let mut senha = String::new(); // Salva senha
+    io::stdin().read_line(&mut senha).unwrap();
 
-    // 6. === Descriptografia ===
-    println!("Descriptografando arquivo...");
+    let mut f_in = File::open(caminho).unwrap(); // Abre arquivo passado
 
-    while let Ok(n) = file_in_cript.read(&mut buffer) {
-        if n == 0 { break; } // Fim do arquivo
+    let mut salt = [0u8; 16]; // Valor fixo para salt (PBKDF2)
+    let mut iv = [0u8; 16]; // Valor fixo IV (CBC)
 
-        add_round_key(&mut buffer, &round_keys[14]); // Adição da chave inicial
+    // Lê os primeiros 16 bytes do arquivo para o Salt e os próximo 16 bytes para o IV
+    // (Para gerar a mesma chave)
+    f_in.read_exact(&mut salt).unwrap(); 
+    f_in.read_exact(&mut iv).unwrap();
 
-            // ---13 rodadas
-        for i in (1..14).rev() {
+    // Gera chave de 256 bits a partir da senha
+    let chave = pbkdf2(senha.trim(), &salt, 100_000);
+    let rkeys = key_expansion(&chave); // Gera as mesmas 15 subchaves
+
+    // Cria o arquivo de saída
+    let mut f_out = File::create(caminho.replace(".cifrado", ".decifrado")).unwrap();
+
+    let mut buffer = [0u8; 16];
+    let mut iv_corr = iv;
+    
+    // Lê o arquivo em pedaços de 16 bytes
+    while let Ok(n) = f_in.read(&mut buffer){
+        if n == 0{
+            break;
+        }
+        let next_iv = buffer; // Será utilizado para o próximo bloco
+
+        // Começa de trás pra frente (aplica rodada 14)
+        add_round_key(&mut buffer, &rkeys[14]);
+
+        // Descriptogração 
+        // Rodada 13 até 1
+        for i in (1..14).rev(){
             inv_sub_bytes(&mut buffer);
             inv_shift_rows(&mut buffer);
-            add_round_key(&mut buffer, &round_keys[i]);
+            add_round_key(&mut buffer, &rkeys[i]);
             inv_mix_columns(&mut buffer);
         }
 
-            // ---rodada final (sem mixcolumn)
+        // Rodada Final
         inv_sub_bytes(&mut buffer);
         inv_shift_rows(&mut buffer);
-        add_round_key(&mut buffer, &round_keys[0]);
+        add_round_key(&mut buffer, &rkeys[0]);
 
-            // 7. Salvar resultado .txt
-        file_out.write_all(&buffer).expect("Falha ao escrever um arquivo.");
+        for i in 0..16{
+            buffer[i] ^= iv_corr[i]; // XOR
+        }
+        iv_corr = next_iv; // Para o próx bloco
+
+        f_out.write_all(&buffer).unwrap(); // Grava os 16 bytes recuperados
     }
-    println!("Descriptografado com sucesso em 'mensagem_descriptografada.txt'!");
+    println!("Decifrado!");
+}
+
+fn testar(){
+    let chave = [0u8; 32]; // Define uma chave nula de 256bits 
+    let mut bloco = [0u8; 16]; // Define um texto nulo de 16 bytes
+
+    // Bloco resultante de zeros
+    let esperado = [
+        0xdc, 0x95, 0xc0, 0x78, 0xa2, 0x40, 0x89, 0x89, 
+        0xad, 0x48, 0xa2, 0x14, 0x92, 0x84, 0x20, 0x87 
+    ];
+
+    let rkeys = key_expansion(&chave); // Expande a chave
+
+    add_round_key(&mut bloco, &rkeys[0]); // Aplica XOR inicial
+
+    // Ciptografar
+
+    // 13 Rodadas
+    for i in 1..14{
+        sub_bytes(&mut bloco);
+        shift_rows(&mut bloco);
+        mix_columns(&mut bloco);
+        add_round_key(&mut bloco, &rkeys[i]);
+    }
+
+    // Rodada final 14
+    sub_bytes(&mut bloco);
+    shift_rows(&mut bloco);
+    add_round_key(&mut bloco, &rkeys[14]);
+
+    println!("Resultado obtido: {:02x?}", bloco); // Exibe o resultado da função
+
+    // Verifica se a implementação deu certo
+    if bloco == esperado{
+        println!("Esperado: {:02x?}", esperado);
+        println!("STATUS: SUCESSO.");
+    } else {
+        println!("STATUS: FALHA");
+        println!("Esperado: {:02x?}", esperado);
+    }
+}
+
+// === MAIN ===
+fn main(){
+    let args: Vec<String> = env::args().collect(); // Captura de argumentos
+
+    match args[1].as_str(){
+        "cifrar" => cifrar(&args[2]), // cargo run cifrar mensagem.txt
+        "decifrar" => decifrar(&args[2]), // cargo run decifrar mensagem.txt.cifrado
+        "testar" => testar(), // cargo run testar
+        _ => (),
+    }
 }
